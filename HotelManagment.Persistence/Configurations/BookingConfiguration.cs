802a2153a021c9e84cfdd11ddda2e16f143a41be
Aleksa Ilic <aleksalc3@gmail.com>
2023-07-28 13:42:24 +0200

    added EntityFramework and DbContext

---
+++ HotelManagment.Persistence/Configurations/BookingConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using HotelManagmnet.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagment.Persistence.Configurations
+{
+    public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.HasData(
+                new Booking
+                {
+                    Id = 1,
+                    User = null,
+                    Room = null,
+                    DateCreated = DateTime.Now,
+                    DateModified = DateTime.Now,
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddDays(10),
+                }
+            );
+
+            //database restriction example
+            builder.Property(q => q.StartDate)
+                .IsRequired();
+        }
+    }
+}
---
+++ HotelManagment.Persistence/DatabaseContext/HotelDatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using HotelManagmnet.Domain;
+using HotelManagmnet.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagment.Persistence.DatabaseContext
+{
+    public class HotelDatabaseContext : DbContext
+    {
+        public HotelDatabaseContext(DbContextOptions<HotelDatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HotelDatabaseContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+                .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+            {
+                entry.Entity.DateModified = DateTime.Now;
+                if (entry.State == EntityState.Added)
+                { 
+                    entry.Entity.DateCreated=DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
