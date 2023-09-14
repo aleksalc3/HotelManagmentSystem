8872ab2dcdc17dbfbb5b8d47ad028d7a632ad8f9
Aleksa Ilic <aleksalc3@gmail.com>
2023-09-14 15:42:12 +0200

    #Added Security using Identity

---
+++ HotelManagementSystem.Application/Contracts/Identity/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using HotelManagementSystem.Application.Models.Identity;
+
+namespace HotelManagementSystem.Application.Contracts.Identity
+{
+    public interface IUserService
+    {
+        Task<List<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(string userId);
+        public string UserId { get; }
+    }
+}
